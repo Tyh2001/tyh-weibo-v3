0cf5d310832a5bf185806ed46416578f6f8c94a8
unknown <you@example.com>
2021-09-22 14:46:53 +0800

    更新接口细节

--- src/api/follow.js
+++ src/api/follow.js
@@ -44,9 +44,9 @@ export const getFansUserList = data => {
 }
 
 /**
- *
- * @param {} data
- * @returns
+ * 取消关注
+ * @param { object } data 用户 id 和 取消关注人的 id
+ * @returns 提示信息 状态码
  */
 export const deleteFollowUser = data => {
   return require({
